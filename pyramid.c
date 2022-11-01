3205be70e94ea5ac5a5e01b0084b9af9e2e95320
Tdneubeck <tdnkvc@umsystem.edu>
2022-11-01 11:21:35 -0500

    Update pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+
 int main() {
    int i, space, rows, k = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
    for (i = 1; i <= rows; ++i, k = 0) {
       for (space = 1; space <= rows - i; ++space) {
-         printf("My name is Tony Neubeck  ");
+         printf("My name is Tony Neubeck ");
       }
       while (k != 2 * i - 1) {
-         printf("* ");
+         printf("My name is Tony Neubeck ");
          ++k;
       }
       printf("\n");
